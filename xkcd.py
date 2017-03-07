2a3bf8fa9c1b32cc34e7b6ec0ffadbd132be8a29
Aron Lawrence <aron.r.lawrence@wmich.edu>
2017-03-06 19:02:47 -0500

    added error handling for invalid comic number

--- xkcd.py
+++ xkcd.py
@@ -9,11 +9,21 @@ def getStrip(myArg= -1):
     if myArg == -1:
         myArg = str(randomNum())
         xkcd = 'https://xkcd.com/' + myArg
-    else:
+    elif validInput(myArg) == True:
         myArg = str(myArg)
         xkcd = 'https://xkcd.com/' + myArg
+    else:
+        xkcd = "That was not a valid comic number."
     return xkcd
-
+def validInput(arg):
+    try:
+        obj = json.load(urllib2.urlopen("https://xkcd.com/info.0.json"))
+        latest = int(obj["num"])
+        if int(arg) in range(1, latest):
+            return True
+        return False
+    except ValueError:
+        return False
 def randomNum():
     """
     This function will return a random comic number 
@@ -29,6 +39,10 @@ def main():
    p = getStrip()
    print p
    p=getStrip(44)
+   print p
+   p=getStrip(7000)
+   print p
+   p=getStrip("testing")
    print p
    #print random_date()
 
