776d9ccdc8e733850d5a4eee388c422dc9d7cef5
Aron Lawrence <aron.r.lawrence@wmich.edu>
2020-01-27 17:11:29 -0500

    Fix call to getTop

--- bashQuotes.py
+++ bashQuotes.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+"""Summary
+"""
 import pytz
 from datetime import datetime
 from datetime import timedelta
@@ -10,81 +12,132 @@ import urllib2, urllib
 import collections
 import re
 
-def getPost(postNumber = -1):
-    if (postNumber == -1):
+def get_post(post_number=-1):
+    """Summary
+    Args:
+        post_number (TYPE, optional): Description
+    Returns:
+        TYPE: Description
+    """
+    if post_number == -1:
         return random_quote()
-    elif RepresentsInt(postNumber) == True:
-       return getSpecificPost(postNumber)
-    elif (postNumber == 'top'):
-        return randTop()
-    elif RepresentsInt(postNumber) == False:
-        return getKewordPost(postNumber)
-    else:
-        return "That was not a valid input try a number."
+    elif post_number == 'top':
+        return rand_top()
+    elif not represents_int(post_number):
+        return getKewordPost(post_number)
+    elif represents_int(post_number):
+        return getSpecificPost(post_number)
 
 def getKewordPost(keywords):
+    """Summary
+    Args:
+        keywords (TYPE): Description
+    Returns:
+        TYPE: Description
+    """
     query = keywords
     query = urllib.quote_plus(query)
     url = 'http://bash.org/?search=' +query +'&sort=0&show=25'
-    content = urllib2.urlopen(url).readlines()
+    #content = urllib2.urlopen(url).readlines()
     #print content
     r = urllib.urlopen(url).read()
     #print r
     soup = BeautifulSoup(r, "lxml")
 
-    mydivs = soup.findAll("p", { "class" : "qt" })
+    mydivs = soup.findAll("p", {"class" : "qt"})
     for row in mydivs:
         return row.text.encode('utf-8')
     return "No quote found matching that search term..."
 
 def random_quote():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     url = 'http://www.bash.org/?random'
-    content = urllib2.urlopen(url).readlines()
+    #content = urllib2.urlopen(url).readlines()
     #print content
     r = urllib.urlopen(url).read()
     #print r
     soup = BeautifulSoup(r, "lxml")
 
-    mydiv = soup.find("p", { "class" : "qt" })
+    mydiv = soup.find("p", {"class" : "qt"})
     return mydiv.text
 
 def getSpecificPost(post):
+    """Summary
+
+    Args:
+        post (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     url = 'http://www.bash.org/?' + str(post)
-    content = urllib2.urlopen(url).readlines()
+    #content = urllib2.urlopen(url).readlines()
     #print content
     r = urllib.urlopen(url).read()
     #print r
     soup = BeautifulSoup(r, "lxml")
 
-    mydivs = soup.findAll("p", { "class" : "qt" })
+    mydivs = soup.findAll("p", {"class" : "qt"})
     for row in mydivs:
         return row.text.encode('utf-8')
     return "That quote does not exist..."
 
-def randTop():
+def rand_top():
+    """Summary
+
+    Returns:
+        TYPE: Description
+    """
     if random.random() < .5:
         page = 1
     else:
         page = 2
     url = 'http://bash.org/?top' + str(page)
-    content = urllib2.urlopen(url).readlines()
+    #content = urllib2.urlopen(url).readlines()
     #print content
     r = urllib.urlopen(url).read()
     #print r
     soup = BeautifulSoup(r, "lxml")
-    mydivs = soup.findAll("p", { "class" : "qt" })
-    randTop = random.randint(0, 99)
-    return mydivs[randTop].text.encode('utf-8')
-    
-def RepresentsInt(s):
+    mydivs = soup.findAll("p", {"class" : "qt"})
+    rand_top_post = random.randint(0, 99)
+    return mydivs[rand_top_post].text.encode('utf-8')
 
-    try: 
-        int(s)
+def get_HTML(url):
+    """Summary
+    
+    Args:
+        url (TYPE): Description
+    
+    Returns:
+        TYPE: Description
+    """
+    #content = urllib2.urlopen(url).readlines()
+    #print content
+    return urllib.urlopen(url).read()
+    #print r
+
+def represents_int(value):
+    """Summary
+
+    Args:
+        s (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
+    try:
+        int(value)
         return True
     except ValueError:
         return False
 
 def main():
+    """Summary
+    """
     print getKewordPost('twitter')
 
 
