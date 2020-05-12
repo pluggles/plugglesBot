529fa377ced402459c7b4c784f6d515a9c3622b2
Aron Lawrence <aron.r.lawrence@wmich.edu>
2020-05-12 18:10:23 -0400

    adds more logging to alexPhoto

--- alexPhoto.py
+++ alexPhoto.py
@@ -9,12 +9,14 @@ upload specific phots for know users
 default photo for unknow
 give option to uplaod your own image'''
 ImgPath = "AlexPhotos"
-def Alex():
+def Alex():list
 	EnsureDirExists(ImgPath)
 	if os.listdir(ImgPath) == []:
 		return 'No Photos Exist'
 	else:
-		return random.choice(os.listdir(ImgPath))
+		choice = random.choice(os.listdir(ImgPath))
+		print choice
+		return choice
 		#check if userID has a picuture
 		#is so send that picture back
 		#else send default pictte
@@ -28,11 +30,8 @@ def EnsureFileExists(filename):
     if not os.path.exists(filename):
         return False
     return True
-def deleteApprovalPhoto(userID):
-	if os.path.exists('images/approval_' + str(userID) + '.jpg'):
-		os.remove('images/approval_' + str(userID) + '.jpg')
-		return "your photo has been removed with extreme prejudice"
-	return "you don't have any approval photos added, you can add one with /approvalphoto"
+def AddPhoto(tempPhoto):
+	
 
 def deleteDisapprovalPhoto(userID):
 	if os.path.exists('images/disapproval_' + str(userID) + '.jpg'):
