b6e48f02ac7d6b80165d39f9fda69525f60a39cb
Aron Lawrence <aron.r.lawrence@wmich.edu>
2020-05-17 15:51:24 -0400

    use correct file extentsion for videos

--- plugglesBot.py
+++ plugglesBot.py
@@ -126,7 +126,7 @@ def addAlex(bot, update):
 """
 def alex(bot, update):
     photoPath = alexPhoto.Alex()
-    if photoPath.endswitch(".mov") or photoPath.endswitch(".mp3"):
+    if photoPath.endswitch(".mov") or photoPath.endswitch(".mp4"):
         bot.send_photo(chat_id=update.message.chat_id, video=open(photoPath, 'rb'), supports_streaming=True)
     else:
         bot.send_photo(chat_id=update.message.chat_id, photo=open(photoPath, 'rb'))
